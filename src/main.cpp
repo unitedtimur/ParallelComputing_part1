958a066aae4189a4d7aa38b7a0abcc09e3cc889f
unitedtimur <lihomanov.t@inbox.ru>
2021-02-16 21:33:51 +0700

    Add algorithm

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include "arrayinitializer.h"
 
 int main()
 {
@@ -9,10 +10,31 @@ int main()
 
     size_t threadNumbers = 0;
 
-    std::cout << "enter the number of threads: " << std::flush;
+    std::cout << "Enter the number of threads: " << std::flush;
     std::cin >> threadNumbers;
 
+    const size_t quotient = arraySize / threadNumbers;
+    const size_t remainder = arraySize % threadNumbers;
 
+    std::vector<size_t> array;
+    array.resize(arraySize, 0);
+
+    std::vector<std::thread> threads;
+    threads.reserve(threadNumbers);
+
+    for (size_t i = 0; i < threadNumbers; ++i) {
+        threads.push_back(std::thread(&ArrayInitializer::execute<size_t>, std::ref(array), i,
+                                      quotient, remainder, threadNumbers));
+
+        if (threads.at(i).joinable())
+            threads.at(i).join();
+    }
+
+    for (const auto &it : array) {
+        std::cout << '[' << it  << "] " << std::flush;
+    }
+
+    std::cout << std::endl;
 
     return 0;
 }
